0982b7670cfcae8a48ebe55b32ef049d40815b2d
eminteoman <57944989+eminteoman@users.noreply.github.com>
2020-12-22 00:38:53 +0300

    Create hw1.py

---
+++ hw1.py
@@ -0,0 +1,11 @@
+inp1 = int(input())
+inp2 = int(input())
+inp3 = int(input())
+inp4 = int(input())
+inp5 = int(input())
+print("Value1 is: {}\nValue2 is: {}\nValue3 is: {}\nValue4 is: {}\nValue5 is: {}".format(inp1,inp2,inp3,inp4,inp5))
+print(f'Type of Value1 is: {type(inp1)}\n\
+Type of Value2 is: {type(inp2)}\n\
+Type of Value3 is: {type(inp3)}\n\
+Type of Value4 is: {type(inp4)}\n\
+Type of Value5 is: {type(inp5)}')
